c021b18e4d80bab84ad8f534e1743b7e3607de96
Stafford Horne <shorne@gmail.com>
2018-03-19 22:45:59 +0900

    or1k: Add some basic definitions for regs/storage etc

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ this is the start of a new OpenRISC fsf clean port for gcc.
 ## Todo
 
 - build infra + basic files - DONE
-- register definitions
+- register definitions - DONE
 - calling conventions
 - memory layout
 - load/store
--- gcc/config/or1k/or1k.h
+++ gcc/config/or1k/or1k.h
@@ -21,4 +21,108 @@
 #ifndef GCC_OR1K_H
 #define GCC_OR1K_H
 
+/* Storage layout.  */
+
+#define DEFAULT_SIGNED_CHAR 1
+#define BITS_BIG_ENDIAN 0
+#define BYTES_BIG_ENDIAN 1
+#define WORDS_BIG_ENDIAN 1
+#define BITS_PER_WORD 32
+#define UNITS_PER_WORD 4
+#define POINTER_SIZE 32
+#define BIGGEST_ALIGNMENT 32
+#define STRICT_ALIGNMENT 1
+#define FUNCTION_BOUNDARY 32
+#define PARM_BOUNDARY 32
+#define STACK_BOUNDARY 32
+#define PREFERRED_STACK_BOUNDARY 32
+#define MAX_FIXED_MODE_SIZE 64
+
+/* Layout of source language data types.  */
+
+#define INT_TYPE_SIZE 32
+#define SHORT_TYPE_SIZE 16
+#define LONG_TYPE_SIZE 32
+#define LONG_LONG_TYPE_SIZE 64
+#define FLOAT_TYPE_SIZE 32
+#define DOUBLE_TYPE_SIZE 64
+#define LONG_DOUBLE_TYPE_SIZE DOUBLE_TYPE_SIZE
+
+#undef SIZE_TYPE
+#define SIZE_TYPE "unsigned int"
+
+#undef PTRDIFF_TYPE
+#define PTRDIFF_TYPE "int"
+
+/* Describing Relative Costs of Operations.  */
+#define MOVE_MAX 4
+#define SLOW_BYTE_ACCESS 1
+
+/* Register usage, class and contents.  */
+
+#define FIRST_PSEUDO_REGISTER  32
+
+#define ZERO_REGNUM   0
+#define SP_REGNUM   1
+#define FP_REGNUM   2
+#define LR_REGNUM   10
+#define TLS_REGNUM  11
+#define RV_REGNUM   12
+#define RVH_REGNUM  13
+
+#define FIXED_REGISTERS   \
+{ 1, 1, 0, 0, 0, 0, 0, 0, \
+  0, 0, 1, 0, 0, 0, 0, 0, \
+  0, 0, 0, 0, 0, 0, 0, 0, \
+  0, 0, 0, 0, 0, 0, 0, 0}
+
+#define CALL_USED_REGISTERS \
+{ 1, 1, 0, 0, 0, 0, 0, 0, \
+  0, 1, 1, 1, 1, 0, 0, 0, \
+  0, 0, 0, 0, 0, 0, 0, 0, \
+  0, 0, 0, 0, 0, 0, 0, 0}
+
+enum reg_class
+{
+  NO_REGS,
+  GENERAL_REGS,
+  ALL_REGS,
+  LIM_REG_CLASSES
+};
+
+#define N_REG_CLASSES (int) LIM_REG_CLASSES
+
+#define REG_CLASS_NAMES { "NO_REGS", "GENERAL_REGS", "ALL_REGS" }
+
+#define REG_CLASS_CONTENTS      \
+{ {0x00000000},                 \
+  {0xffffffff},                 \
+  {0xffffffff}                  \
+}
+
+/* Assembly definitions.  */
+
+#define ASM_APP_ON "#APP\n"
+#define ASM_APP_OFF "#NO_APP\n"
+
+#define ASM_COMMENT_START "# "
+
+#define GLOBAL_ASM_OP "\t.global\t"
+
+#define REGISTER_NAMES {						\
+  "r0",   "r1",   "r2",   "r3",   "r4",   "r5",   "r6",   "r7",		\
+  "r8",   "r9",   "r10",  "r11",  "r12",  "r13",  "r14",  "r15",	\
+  "r16",  "r17",  "r18",  "r19",  "r20",  "r21",  "r22",  "r23",	\
+  "r24",  "r25",  "r26",  "r27",  "r28",  "r29",  "r30",  "r31" 	\
+}
+
+/* Calling convention definitions.  */
+typedef struct or1k_args
+{
+  int regs_used;
+} CUMULATIVE_ARGS;
+
+#define INIT_CUMULATIVE_ARGS(CUM, FNTYPE, LIBNAME, FNDECL, N_NAMED_ARGS) \
+  do { (CUM).regs_used = 0; } while (0)
+
 #endif /* GCC_OR1K_H */
